ba6e585bb914d6e13492c6a9aee914cecd49d0cf
Nicholas Feinberg <pleasingfung@gmail.com>
2013-02-17 12:19:30 -0800

    Added profile-help batch script

---
+++ telem.bat
@@ -0,0 +1,3 @@
+C:\Python27\python.exe "C:\Users\Nick\My Dropbox\Code\telemetry-utils-master\add-opt-in.py" "C:\Users\Nick\My Dropbox\Code\MDE2\bin\MDE2.swf"
+PING 1.1.1.1 -n 1 -w 500 >NUL
+start "" "C:\Users\Nick\My Dropbox\Code\MDE2\bin\MDE2.swf"
